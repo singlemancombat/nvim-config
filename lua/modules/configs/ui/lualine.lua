2475920f6c4d392b9c935fdfe7a2a9cd1c2d0942
singlemancombat <freeworld@freeworld.local>
2023-03-05 00:14:27 -0800

    Use catppuccin theme for lualine.

--- lua/modules/configs/ui/lualine.lua
+++ lua/modules/configs/ui/lualine.lua
@@ -104,7 +104,7 @@ return function()
   require("lualine").setup({
     options = {
       icons_enabled = true,
-      theme = "kanagawa",
+      theme = "catppuccin",
       disabled_filetypes = {},
       component_separators = "|",
       section_separators = { left = "", right = "" },
