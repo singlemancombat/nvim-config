0f3fb99d972e51e885d23974ecd084af40b1b7df
singlemancombat <freeworld@freeworld.local>
2023-03-07 22:12:10 -0800

    Update lualine separator style.

--- lua/modules/configs/ui/lualine.lua
+++ lua/modules/configs/ui/lualine.lua
@@ -106,8 +106,16 @@ return function()
       icons_enabled = true,
       theme = "catppuccin",
       disabled_filetypes = {},
-      component_separators = "|",
-      section_separators = { left = "", right = "" },
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+      ignore_focus = {},
+      always_divide_middle = true,
+      globalstatus = false,
+      refresh = {
+        statusline = 1000,
+        tabline = 1000,
+        winbar = 1000,
+      },
     },
     sections = {
       lualine_a = { { "mode" } },
