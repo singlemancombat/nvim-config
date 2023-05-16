8bf8b84fa6fb34514c938db082936f46d7ab0cd2
ayamir <lgt986452565@gmail.com>
2023-05-16 11:43:48 +0800

    chore(wsl): use Windows native way to yank and paste. (#736)
    * chore(wsl): use Windows native way to yank and paste.
    
    * style: update code style.
    
    Co-authored-by: 冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: 冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -92,27 +92,27 @@ local neovide_config = function()
 end
 
 local clipboard_config = function()
-  if global.is_mac then
-    vim.g.clipboard = {
-      name = "macOS-clipboard",
-      copy = { ["+"] = "pbcopy", ["*"] = "pbcopy" },
-      paste = { ["+"] = "pbpaste", ["*"] = "pbpaste" },
-      cache_enabled = 0,
-    }
-  elseif global.is_wsl then
-    vim.g.clipboard = {
-      name = "win32yank-wsl",
-      copy = {
-        ["+"] = "win32yank.exe -i --crlf",
-        ["*"] = "win32yank.exe -i --crlf",
-      },
-      paste = {
-        ["+"] = "win32yank.exe -o --lf",
-        ["*"] = "win32yank.exe -o --lf",
-      },
-      cache_enabled = 0,
-    }
-  end
+	if global.is_mac then
+		vim.g.clipboard = {
+			name = "macOS-clipboard",
+			copy = { ["+"] = "pbcopy", ["*"] = "pbcopy" },
+			paste = { ["+"] = "pbpaste", ["*"] = "pbpaste" },
+			cache_enabled = 0,
+		}
+	elseif global.is_wsl then
+		vim.g.clipboard = {
+			name = "psyank-wsl",
+			copy = {
+				["+"] = "clip.exe",
+				["*"] = "clip.exe",
+			},
+			paste = {
+				["+"] = [[powershell.exe -NoProfile -NoLogo -NonInteractive -MTA -Command [console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))]],
+				["*"] = [[powershell.exe -NoProfile -NoLogo -NonInteractive -MTA -Command [console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))]],
+			},
+			cache_enabled = 0,
+		}
+	end
 end
 
 local shell_config = function()
