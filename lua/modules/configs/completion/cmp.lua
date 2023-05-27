2f3c9516a799fde13a1bba12f63ac5648b2790e3
ayamir <lgt986452565@gmail.com>
2023-05-27 21:52:36 +0800

    fix: adapt to cmp interface change, set max_view_entries instead. (#768)
    * fix: adapt to cmp interface change, set max_view_entries instead.
    
    * fixup! fix: adapt to cmp interface change, set max_view_entries instead.
    
    ---------
    
    Co-authored-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/completion/cmp.lua
+++ lua/modules/configs/completion/cmp.lua
@@ -61,116 +61,120 @@ return function()
     end
   end
 
-  local cmp = require("cmp")
-  cmp.setup({
-    preselect = cmp.PreselectMode.Item,
-    window = {
-      completion = {
-        border = border("Normal"),
-        max_width = 80,
-        max_height = 20,
-        scrollbar = false,
-      },
-      documentation = {
-        border = border("CmpDocBorder"),
-      },
-    },
-    sorting = {
-      priority_weight = 2,
-      comparators = {
-        require("copilot_cmp.comparators").prioritize,
-        require("copilot_cmp.comparators").score,
-        -- require("cmp_tabnine.compare"),
-        compare.offset, -- Items closer to cursor will have lower priority
-        compare.exact,
-        -- compare.scopes,
-        compare.lsp_scores,
-        compare.sort_text,
-        compare.score,
-        compare.recently_used,
-        -- compare.locality, -- Items closer to cursor will have higher priority, conflicts with `offset`
-        require("cmp-under-comparator").under,
-        compare.kind,
-        compare.length,
-        compare.order,
-      },
-    },
-    formatting = {
-      fields = { "menu", "abbr", "kind" },
-      format = function(entry, vim_item)
-        local kind_map = vim.tbl_deep_extend("force", icons.kind, icons.type, icons.cmp)
-        local kind = cmp_format({
-          maxwidth = 50,
-          symbol_map = kind_map,
-        })(entry, vim_item)
-        return kind
-      end,
-    },
-    matching = {
-      disallow_partial_fuzzy_matching = false,
-    },
-    -- You can set mappings if you want
-    mapping = cmp.mapping.preset.insert({
-      ["<CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
-      ["<C-p>"] = cmp.mapping.select_prev_item(),
-      ["<C-n>"] = cmp.mapping.select_next_item(),
-      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-w>"] = cmp.mapping.close(),
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif require("luasnip").expand_or_locally_jumpable() then
-          vim.fn.feedkeys(t("<Plug>luasnip-expand-or-jump"))
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif require("luasnip").jumpable(-1) then
-          vim.fn.feedkeys(t("<Plug>luasnip-jump-prev"), "")
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-    }),
-    snippet = {
-      expand = function(args)
-        require("luasnip").lsp_expand(args.body)
-      end,
-    },
-    -- You should specify your *installed* sources.
-    sources = {
-      { name = "nvim_lsp", max_item_count = 350 },
-      { name = "nvim_lua" },
-      { name = "luasnip" },
-      { name = "path" },
-      {
-        name = "treesitter",
-        entry_filter = function(entry)
-          local ignore_list = {
-            "Error",
-            "Comment",
-          }
-          local kind = entry:get_completion_item().cmp.kind_text
-          return not vim.tbl_contains(ignore_list, kind)
-        end,
-      },
-      { name = "spell" },
-      { name = "tmux" },
-      { name = "orgmode" },
-      { name = "buffer" },
-      { name = "latex_symbols" },
-      { name = "copilot" },
-      -- { name = "codeium" },
-      -- { name = "cmp_tabnine" },
-    },
-    experimental = {
-      ghost_text = {
-        hl_group = "Whitespace",
-      },
-    },
-  })
+	local cmp = require("cmp")
+	cmp.setup({
+		preselect = cmp.PreselectMode.Item,
+		window = {
+			completion = {
+				border = border("Normal"),
+				max_width = 80,
+				max_height = 20,
+				scrollbar = false,
+			},
+			documentation = {
+				border = border("CmpDocBorder"),
+			},
+		},
+		sorting = {
+			priority_weight = 2,
+			comparators = {
+				require("copilot_cmp.comparators").prioritize,
+				require("copilot_cmp.comparators").score,
+				-- require("cmp_tabnine.compare"),
+				compare.offset, -- Items closer to cursor will have lower priority
+				compare.exact,
+				-- compare.scopes,
+				compare.lsp_scores,
+				compare.sort_text,
+				compare.score,
+				compare.recently_used,
+				-- compare.locality, -- Items closer to cursor will have higher priority, conflicts with `offset`
+				require("cmp-under-comparator").under,
+				compare.kind,
+				compare.length,
+				compare.order,
+			},
+		},
+		formatting = {
+			fields = { "menu", "abbr", "kind" },
+			format = function(entry, vim_item)
+				local kind_map = vim.tbl_deep_extend("force", icons.kind, icons.type, icons.cmp)
+				local kind = cmp_format({
+					maxwidth = 50,
+					symbol_map = kind_map,
+				})(entry, vim_item)
+				return kind
+			end,
+		},
+		matching = {
+			disallow_partial_fuzzy_matching = false,
+		},
+		performance = {
+			async_budget = 1,
+			max_view_entries = 300,
+		},
+		-- You can set mappings if you want
+		mapping = cmp.mapping.preset.insert({
+			["<CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
+			["<C-p>"] = cmp.mapping.select_prev_item(),
+			["<C-n>"] = cmp.mapping.select_next_item(),
+			["<C-d>"] = cmp.mapping.scroll_docs(-4),
+			["<C-f>"] = cmp.mapping.scroll_docs(4),
+			["<C-w>"] = cmp.mapping.close(),
+			["<Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_next_item()
+				elseif require("luasnip").expand_or_locally_jumpable() then
+					vim.fn.feedkeys(t("<Plug>luasnip-expand-or-jump"))
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
+			["<S-Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_prev_item()
+				elseif require("luasnip").jumpable(-1) then
+					vim.fn.feedkeys(t("<Plug>luasnip-jump-prev"), "")
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
+		}),
+		snippet = {
+			expand = function(args)
+				require("luasnip").lsp_expand(args.body)
+			end,
+		},
+		-- You should specify your *installed* sources.
+		sources = {
+			{ name = "nvim_lsp" },
+			{ name = "nvim_lua" },
+			{ name = "luasnip" },
+			{ name = "path" },
+			{
+				name = "treesitter",
+				entry_filter = function(entry)
+					local ignore_list = {
+						"Error",
+						"Comment",
+					}
+					local kind = entry:get_completion_item().cmp.kind_text
+					return not vim.tbl_contains(ignore_list, kind)
+				end,
+			},
+			{ name = "spell" },
+			{ name = "tmux" },
+			{ name = "orgmode" },
+			{ name = "buffer" },
+			{ name = "latex_symbols" },
+			{ name = "copilot" },
+			-- { name = "codeium" },
+			-- { name = "cmp_tabnine" },
+		},
+		experimental = {
+			ghost_text = {
+				hl_group = "Whitespace",
+			},
+		},
+	})
 end
