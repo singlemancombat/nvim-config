7845139336797ba19db002df6b2a755879e45a51
Vincent Wang <vincew.0927@gmail.com>
2023-06-14 22:57:39 -0700

    Remove duplicate code in ui plugins.

--- lua/modules/plugins/ui.lua
+++ lua/modules/plugins/ui.lua
@@ -94,40 +94,6 @@ ui["EdenEast/nightfox.nvim"] = {
   name = "nightfox",
   config = require("ui.nightfox"),
 }
-ui["sainnhe/edge"] = {
-  lazy = true,
-  config = require("ui.edge"),
-}
-ui["j-hui/fidget.nvim"] = {
-  lazy = true,
-  event = "BufReadPost",
-  config = require("ui.fidget"),
-}
-ui["lewis6991/gitsigns.nvim"] = {
-  lazy = true,
-  event = { "CursorHold", "CursorHoldI" },
-  config = require("ui.gitsigns"),
-}
-ui["lukas-reineke/indent-blankline.nvim"] = {
-  lazy = true,
-  event = "BufReadPost",
-  config = require("ui.indent-blankline"),
-}
-ui["nvim-lualine/lualine.nvim"] = {
-  lazy = true,
-  event = { "BufReadPost", "BufAdd", "BufNewFile" },
-  config = require("ui.lualine"),
-}
-ui["zbirenbaum/neodim"] = {
-  lazy = true,
-  event = "LspAttach",
-  config = require("ui.neodim"),
-}
-ui["karb94/neoscroll.nvim"] = {
-  lazy = true,
-  event = "BufReadPost",
-  config = require("ui.neoscroll"),
-}
 ui["rcarriga/nvim-notify"] = {
   lazy = true,
   event = "VeryLazy",
