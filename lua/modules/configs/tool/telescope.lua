c68995bca8163ad04b4763920e907d0933ac2556
ayamir <lgt986452565@gmail.com>
2023-06-22 21:24:43 +0800

    feat: use persisted to provide more powerful session management. (#810)
    * feat: use persisted to provide more powerful session management.
    
    * fixup! feat: use persisted to provide more powerful session management.
    
    ---------
    
    Co-authored-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/tool/telescope.lua
+++ lua/modules/configs/tool/telescope.lua
@@ -133,4 +133,5 @@ return function()
   require("telescope").load_extension("projects")
   require("telescope").load_extension("undo")
   require("telescope").load_extension("zoxide")
+  require("telescope").load_extension("persisted")
 end
