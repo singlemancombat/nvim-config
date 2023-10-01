8943ca96b7bbc3cf3d38bb3d71de9ad0a4895732
Charles Chiu <charleschiu@hey.com>
2023-10-01 05:03:38 -0500

    feat(ibl): support `indent-blankline` v3 (#1011)
    * migrate(ibl): `indent-blankline` to v3
    
    * migrate(ibl): draft v3 config
    
    * feat(ibl): plugins support & misc updates
    
    * fix merge conflicts
    
    ---------
    
    Co-authored-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/ui/catppuccin.lua
+++ lua/modules/configs/ui/catppuccin.lua
@@ -117,9 +117,9 @@ return function()
           -- For mason.nvim
           MasonNormal = { link = "NormalFloat" },
 
-          -- For indent-blankline
-          IndentBlanklineChar = { fg = cp.surface0 },
-          IndentBlanklineContextChar = { fg = cp.surface2, style = { "bold" } },
+		  -- For indent-blankline
+		  IblIndent = { fg = cp.surface0 },
+		  IblScope = { fg = cp.surface2, style = { "bold" } },
 
           -- For nvim-cmp and wilder.nvim
           Pmenu = { fg = cp.overlay2, bg = transparent_background and cp.none or cp.base },
