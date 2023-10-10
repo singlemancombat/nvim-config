ae14643888a6183fd4bf137d532a1fe7dc6f0a3e
Vincent Wang <vincew.0927@gmail.com>
2023-10-09 22:18:06 -0700

    Add leetcode plugin.

--- lua/modules/plugins/tool.lua
+++ lua/modules/plugins/tool.lua
@@ -1,5 +1,36 @@
 local tool = {}
 
+---@alias lc.lang
+---| "cpp"
+---| "java"
+---| "python"
+---| "python3"
+---| "c"
+---| "csharp"
+---| "javascript"
+---| "typescript"
+---| "php"
+---| "swift"
+---| "kotlin"
+---| "dart"
+---| "golang"
+---| "ruby"
+---| "scala"
+---| "rust"
+---| "racket"
+---| "erlang"
+---| "elixir"
+
+---@alias lc.sql_lang
+---| "pythondata"
+---| "mysql"
+---| "mssql"
+---| "oraclesql"
+
+---@alias lc.domain
+---| "com"
+---| "cn"
+
 tool["tpope/vim-fugitive"] = {
   lazy = true,
   cmd = { "Git", "G" },
@@ -131,6 +162,48 @@ tool["nvim-neorg/neorg"] = {
   build = ":Neorg sync-parsers",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = require("tool.neorg"),
+}
+
+----------------------------------------------------------------------
+--                              LeetCode                            --
+----------------------------------------------------------------------
+tool["kawre/leetcode.nvim"] = {
+  build = ":TSUpdate html",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim", -- required by telescope
+    "MunifTanjim/nui.nvim",
+    -- optional
+    "nvim-tree/nvim-web-devicons",
+    -- recommended
+    "rcarriga/nvim-notify",
+  },
+  opts = {
+    -- configuration goes here
+    ---@type lc.domain
+    domain = "com", -- For now "com" is the only one supported
+    ---@type string
+    arg = "leetcode.nvim",
+    ---@type lc.lang
+    lang = "golang",
+    ---@type lc.sql_lang
+    sql = "mysql",
+    ---@type string
+    directory = vim.fn.stdpath("data") .. "/leetcode/",
+    ---@type boolean
+    logging = true,
+    console = {
+      size = {
+        width = "75%", ---@type string | integer
+        height = "75%", ---@type string | integer
+      },
+      dir = "row", ---@type "col" | "row"
+    },
+    description = {
+      width = "40%", ---@type string | integer
+    },
+  },
 }
 
 return tool
