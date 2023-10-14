9d4e0e6ca1223ad8f2c3cf5a2ff2acdb82f6de33
Charles Chiu <charleschiu@hey.com>
2023-10-14 20:58:14 +0800

    refactor(telescope-frecency)!: remove support for `sqlite.lua` (#1019)
    * migrate(tele-frecency): remove `sqlite.lua` dependency
    
    Signed-off-by: Charles Chiu <charleschiu@hey.com>
    
    * chore: completely remove everything w/ sqlite.lua
    
    ---------
    
    Signed-off-by: Charles Chiu <charleschiu@hey.com>
    Co-authored-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/plugins/tool.lua
+++ lua/modules/plugins/tool.lua
@@ -103,25 +103,23 @@ tool["alexghergh/nvim-tmux-navigation"] = {
 --                        Telescope Plugins                         --
 ----------------------------------------------------------------------
 tool["nvim-telescope/telescope.nvim"] = {
-  lazy = true,
-  cmd = "Telescope",
-  config = require("tool.telescope"),
-  dependencies = {
-    { "nvim-tree/nvim-web-devicons" },
-    { "nvim-lua/plenary.nvim" },
-    { "debugloop/telescope-undo.nvim" },
-    {
-      "ahmedkhalf/project.nvim",
-      event = { "CursorHold", "CursorHoldI" },
-      config = require("tool.project"),
-    },
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    { "nvim-telescope/telescope-frecency.nvim", dependencies = {
-      { "kkharji/sqlite.lua" },
-    } },
-    { "jvgrootveld/telescope-zoxide" },
-    { "nvim-telescope/telescope-live-grep-args.nvim" },
-  },
+	lazy = true,
+	cmd = "Telescope",
+	config = require("tool.telescope"),
+	dependencies = {
+		{ "nvim-tree/nvim-web-devicons" },
+		{ "nvim-lua/plenary.nvim" },
+		{ "debugloop/telescope-undo.nvim" },
+		{
+			"ahmedkhalf/project.nvim",
+			event = { "CursorHold", "CursorHoldI" },
+			config = require("tool.project"),
+		},
+		{ "jvgrootveld/telescope-zoxide" },
+		{ "nvim-telescope/telescope-frecency.nvim" },
+		{ "nvim-telescope/telescope-live-grep-args.nvim" },
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
 }
 
 ----------------------------------------------------------------------
