8da41f77115ea86eea3365aeb6f2bb5fdd87b385
YuCao16 <62466929+YuCao16@users.noreply.github.com>
2023-11-01 11:16:54 +0000

    feat(plugins): add `flash.nvim` to replace `clever-f.vim` (#926)
    * disable enforce_regular_tabs to fix unique name not working
    
    * add flash.nvim
    
    * disable multi_window
    
    * fixup!: remove comment lines in keymap, add NOTEs for users.
    
    Signed-off-by: ayamir <lgt986452565@gmail.com>
    
    * move flash_ecs to keymap/helpers.lua and rename to _flash_esc
    
    * feat(flash): support user-override
    
    * pref: explicitly disable `flash.search` by default
    
    * chore: sort keymap helpers
    
    * fix typo
    
    * fixup! fix typo
    
    ---------
    
    Signed-off-by: ayamir <lgt986452565@gmail.com>
    Co-authored-by: ayamir <lgt986452565@gmail.com>
    Co-authored-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/core/mapping.lua
+++ lua/core/mapping.lua
@@ -2,20 +2,23 @@ local bind = require("keymap.bind")
 local map_cr = bind.map_cr
 local map_cu = bind.map_cu
 local map_cmd = bind.map_cmd
+local map_callback = bind.map_callback
 
 local core_map = {
   -- Suckless
   ["n|<S-Tab>"] = map_cr("normal za"):with_noremap():with_silent():with_desc("edit: Toggle code fold"),
   ["n|<C-s>"] = map_cu("write"):with_noremap():with_silent():with_desc("edit: Save file"),
-  ["n|<C-S-s>"] = map_cmd("execute 'silent! write !sudo tee % >/dev/null' <bar> edit!")
-    :with_silent()
-    :with_noremap()
-    :with_desc("edit: Save file using sudo"),
   ["n|Y"] = map_cmd("y$"):with_desc("edit: Yank text to EOL"),
   ["n|D"] = map_cmd("d$"):with_desc("edit: Delete text to EOL"),
   ["n|n"] = map_cmd("nzzzv"):with_noremap():with_desc("edit: Next search result"),
   ["n|N"] = map_cmd("Nzzzv"):with_noremap():with_desc("edit: Prev search result"),
   ["n|J"] = map_cmd("mzJ`z"):with_noremap():with_desc("edit: Join next line"),
+  ["n|<Esc>"] = map_callback(function()
+      _flash_esc_or_noh()
+    end)
+    :with_noremap()
+    :with_silent()
+    :with_desc("edit: Clear search highlight"),
   ["n|<C-h>"] = map_cmd("<C-w>h"):with_noremap():with_desc("window: Focus left"),
   ["n|<C-l>"] = map_cmd("<C-w>l"):with_noremap():with_desc("window: Focus right"),
   ["n|<C-j>"] = map_cmd("<C-w>j"):with_noremap():with_desc("window: Focus down"),
@@ -28,10 +31,13 @@ local core_map = {
   ["n|<A-]>"] = map_cr("vertical resize +5"):with_silent():with_desc("window: Resize +5 vertically"),
   ["n|<A-;>"] = map_cr("resize -2"):with_silent():with_desc("window: Resize -2 horizontally"),
   ["n|<A-'>"] = map_cr("resize +2"):with_silent():with_desc("window: Resize +2 horizontally"),
-  ["n|<leader>bc"] = map_cmd("<cmd>BufDel<CR>"):with_noremap():with_desc("buffer: Close"),
-  ["n|<C-q>"] = map_cmd(":wq<CR>"):with_desc("edit: Save file and quit"),
-  ["n|<A-S-q>"] = map_cmd(":q!<CR>"):with_desc("edit: Force quit"),
+  ["n|<C-q>"] = map_cr("wq"):with_desc("edit: Save file and quit"),
+  ["n|<A-S-q>"] = map_cr("q!"):with_desc("edit: Force quit"),
   ["n|<leader>o"] = map_cr("setlocal spell! spelllang=en_us"):with_desc("edit: Toggle spell check"),
+  ["n|tn"] = map_cr("tabnew"):with_noremap():with_silent():with_desc("tab: Create a new tab"),
+  ["n|tk"] = map_cr("tabnext"):with_noremap():with_silent():with_desc("tab: Move to next tab"),
+  ["n|tj"] = map_cr("tabprevious"):with_noremap():with_silent():with_desc("tab: Move to previous tab"),
+  ["n|to"] = map_cr("tabonly"):with_noremap():with_silent():with_desc("tab: Only keep current tab"),
   -- Insert mode
   ["i|<C-u>"] = map_cmd("<C-G>u<C-U>"):with_noremap():with_desc("edit: Delete previous block"),
   ["i|<C-b>"] = map_cmd("<Left>"):with_noremap():with_desc("edit: Move cursor to left"),
