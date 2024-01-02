99432b29533405f7a03889616cdebfe0a1a4be18
Mohu <lgt986452565@gmail.com>
2024-01-02 10:11:19 +0800

    fix(cmp): don't auto select the first completion candidate (#1118)

--- lua/modules/configs/completion/cmp.lua
+++ lua/modules/configs/completion/cmp.lua
@@ -118,68 +118,68 @@ return function()
           vim_item.abbr = truncated_label .. "..."
         end
 
-        return vim_item
-      end,
-    },
-    matching = {
-      disallow_partial_fuzzy_matching = false,
-    },
-    performance = {
-      async_budget = 1,
-      max_view_entries = 120,
-    },
-    -- You can set mappings if you want
-    mapping = cmp.mapping.preset.insert({
-      ["<CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
-      ["<C-p>"] = cmp.mapping.select_prev_item(),
-      ["<C-n>"] = cmp.mapping.select_next_item(),
-      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-w>"] = cmp.mapping.close(),
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif require("luasnip").expand_or_locally_jumpable() then
-          vim.fn.feedkeys(t("<Plug>luasnip-expand-or-jump"))
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif require("luasnip").jumpable(-1) then
-          vim.fn.feedkeys(t("<Plug>luasnip-jump-prev"), "")
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-    }),
-    snippet = {
-      expand = function(args)
-        require("luasnip").lsp_expand(args.body)
-      end,
-    },
-    -- You should specify your *installed* sources.
-    sources = {
-      { name = "nvim_lsp", max_item_count = 350 },
-      { name = "nvim_lua" },
-      { name = "luasnip" },
-      { name = "path" },
-      { name = "treesitter" },
-      { name = "spell" },
-      { name = "tmux" },
-      { name = "orgmode" },
-      { name = "buffer" },
-      { name = "latex_symbols" },
-      { name = "copilot" },
-      -- { name = "codeium" },
-      -- { name = "cmp_tabnine" },
-    },
-    experimental = {
-      ghost_text = {
-        hl_group = "Whitespace",
-      },
-    },
-  })
+				return vim_item
+			end,
+		},
+		matching = {
+			disallow_partial_fuzzy_matching = false,
+		},
+		performance = {
+			async_budget = 1,
+			max_view_entries = 120,
+		},
+		-- You can set mappings if you want
+		mapping = cmp.mapping.preset.insert({
+			["<CR>"] = cmp.mapping.confirm({ select = false, behavior = cmp.ConfirmBehavior.Replace }),
+			["<C-p>"] = cmp.mapping.select_prev_item(),
+			["<C-n>"] = cmp.mapping.select_next_item(),
+			["<C-d>"] = cmp.mapping.scroll_docs(-4),
+			["<C-f>"] = cmp.mapping.scroll_docs(4),
+			["<C-w>"] = cmp.mapping.close(),
+			["<Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_next_item()
+				elseif require("luasnip").expand_or_locally_jumpable() then
+					vim.fn.feedkeys(t("<Plug>luasnip-expand-or-jump"))
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
+			["<S-Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_prev_item()
+				elseif require("luasnip").jumpable(-1) then
+					vim.fn.feedkeys(t("<Plug>luasnip-jump-prev"), "")
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
+		}),
+		snippet = {
+			expand = function(args)
+				require("luasnip").lsp_expand(args.body)
+			end,
+		},
+		-- You should specify your *installed* sources.
+		sources = {
+			{ name = "nvim_lsp", max_item_count = 350 },
+			{ name = "nvim_lua" },
+			{ name = "luasnip" },
+			{ name = "path" },
+			{ name = "treesitter" },
+			{ name = "spell" },
+			{ name = "tmux" },
+			{ name = "orgmode" },
+			{ name = "buffer" },
+			{ name = "latex_symbols" },
+			{ name = "copilot" },
+			-- { name = "codeium" },
+			-- { name = "cmp_tabnine" },
+		},
+		experimental = {
+			ghost_text = {
+				hl_group = "Whitespace",
+			},
+		},
+	})
 end
